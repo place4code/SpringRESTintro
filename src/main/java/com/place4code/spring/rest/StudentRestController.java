4d16682a3131f91d15d7034a8f1c9963117efed3
place4code <place4code@gmail.com>
2019-04-10 14:14:12 +0200

    intro Exception handling

--- src/main/java/com/place4code/spring/rest/StudentRestController.java
+++ src/main/java/com/place4code/spring/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,28 @@ public class StudentRestController {
 
 	@GetMapping("students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+		
+		//if student doesn't exist
+		if (studentId >= students.size() || studentId < 0) 
+			throw new StudentNotFoundException("Student not found: " + studentId);
+		
 		return students.get(studentId);
 	}
+	
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handle(StudentNotFoundException e) {
+		
+		//create a StudentErrorResponse
+		StudentErrorResponse errorResponse = new StudentErrorResponse(
+				e.getMessage(), 
+				System.currentTimeMillis(),
+				HttpStatus.NOT_FOUND.value());
+		
+		//return error response
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+		
+	}
+	
+
 }
